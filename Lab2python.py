bf8d523c2b89bd1bcd9d56268a98b144b00c0a62
JuhnChuan <LJUHNCHUAN.23@ichat.sp.edu.sg>
2025-11-05 10:15:42 +0800

    This Lab2python.py is incomplete

--- Lab2python.py
+++ Lab2python.py
@@ -1,1 +1,34 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu(list):
+    print("Enter some numbers separated by commas (e.g. 5,67,32)")
+
+def get_user_input():
+    x = input()
+    str_list = x.split(",")
+    float_list = []
+    for string in str_list:
+        float_num = float(string)
+        float_list.append(float_num)
+    return(float_list)
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+
+
+def calc_avg(list):
+    print("average")
+
+
+def min_max(list):
+    print("something")
+
+def sort_temp(list):
+    print("list")
+
+def calc_median_temp(list):
+    print("median")
+
+if __name__ == "__main__":
+    main()
--- bmi.py
+++ bmi.py
@@ -5,9 +5,14 @@ def calculate_bmi(height, weight):
     print("BMI is " + str(bmi))
     if bmi < 18.5:
         print("Skinny, bmi is "+ str(bmi))
+        val =  -1
     elif bmi >25.0:
         print("fatty, bmi is "+ str(bmi))
+        val = 1
     else:
         print("normal, bmi is"+ str(bmi))
+        val = 0
+    return val
 
-calculate_bmi(weight=67, height=1.63)
+valu = calculate_bmi(weight=67, height=1.63)
+print(str(valu))
